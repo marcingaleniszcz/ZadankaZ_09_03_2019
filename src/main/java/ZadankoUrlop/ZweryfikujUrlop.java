2639d95f6886928c12c9cf503a77245171ff4712
marcingaleniszcz <48137171+marcingaleniszcz@users.noreply.github.com>
2019-03-09 15:59:55 +0100

    Kolejne zad

--- src/main/java/ZadankoUrlop/ZweryfikujUrlop.java
+++ src/main/java/ZadankoUrlop/ZweryfikujUrlop.java
@@ -1,4 +1,23 @@
 package ZadankoUrlop;
 
-public class ZweryfikujUrlop {
+import java.time.LocalDate;
+import java.util.function.Predicate;
+
+public class ZweryfikujUrlop implements Predicate<Urlop> {
+    private LocalDate data;
+
+    public ZweryfikujUrlop(LocalDate data) {
+        this.data = data;
+    }
+    public boolean test(Urlop urlop){
+        return data != null
+                && !data.isBefore(urlop.getDataOd())
+                && !data.isAfter(urlop.getDataDo());
+    }
+    public LocalDate getData() {
+        return data;
+    }
+    public void setData(LocalDate data){
+        this.data = data;
+    }
 }
